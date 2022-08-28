7d41e0a445dd99bdc521ebbf965d2deef9934e9c
elsateklu7 <elsateklu7@gmail.com>
2022-08-28 21:12:10 +0300

    Get image url

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   /**************************************************************************** */
 
-  app.get("/filtered", async (req: Request, res: Response) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     const image_url = req.query.image_url.toString();
     if (!image_url) {
       res.status(400).send("image url is required");
